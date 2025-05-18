802c3c56faf7bb0b9295cafe3c8b1dd7266bfb94
Raushan kumar <raushan.kumar@tappp.com>
2025-05-18 23:48:42 +0530

    javascripts functions

--- src/advance_js/functionalProgramming/function_js.js
+++ src/advance_js/functionalProgramming/function_js.js
@@ -1,3 +1,4 @@
+"use strict"
 function marry(person1, person2) {
     console.log("arguments", arguments)
     console.log(Array.from(arguments))
@@ -33,3 +34,82 @@ one();
 //  global()=> false;
 
 //  reference some where have memory heap
+function magic() {
+    return x = 1, [], {}; // javascript expression left to right 
+};
+
+console.log(magic())
+
+//  Scope chain
+//  all these function have global lexical environment
+//  all these function have some link is called scope chain, scope chain link these are have access to all function access
+//  all these function have own variable environment 
+//  is called  scope and static scope;
+//  In javascript our lexical scope (available data + variables where the function was defined)
+//  determines our available variables. Not where the function is called (dynamic scope)
+
+var x = "x";
+
+function findName() {
+    console.log(x)
+    var b = "b";
+    return printName();
+};
+
+function printName() {
+    var c = "c";
+    return "Ajay Vineet";
+};
+
+function sayName() {
+    var a = "a";
+    return findName();
+};
+
+let ans = sayName();
+console.log(ans);
+
+//  rin lexical 
+
+function sayNames() {
+    var a = "a";
+    return function findName() {
+        var b = "b";
+        // console.log(c)
+        return function printName() {
+            var c = "c";
+            return "Vineet Ajay DevOp"
+        }
+    }
+};
+
+let res = sayNames()()()
+console.log(res)
+
+//  function Lexical Environment above example is called all lexical environment
+//  Lexical Environment === [[Scope]]
+
+//  exercie
+
+function werid() {
+    // height = 50; // i have to declear var or let const because if we are top our use strict getting reference error height is define that why javascrit behave some time werid
+    var height=50 // now error gone as soon as declear as variable
+    return height;
+
+}
+
+let w = werid();
+// console.log(w)
+
+//  let creating a anothe example for 
+
+var heyhey = function doodle() {
+    //  do some thing
+    //  doodle function acces only here not global excuetion context
+    return "heyhey";
+}
+
+let d = heyhey() // give me result like this heyhey
+// console.log(d);
+//  if directly call doodle then getting error reference error  doodle is not defined
+doodle()
