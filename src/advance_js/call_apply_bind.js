acc09d48aed70f95f683de62be6ef9f12cef629a
Raushan kumar <raushan.kumar@tappp.com>
2025-05-25 17:22:58 +0530

    bind  and currying function

--- src/advance_js/call_apply_bind.js
+++ src/advance_js/call_apply_bind.js
@@ -3,9 +3,8 @@ function a() {
   console.log("Hey");
 }
 a.call();
-
 const wizard = {
-  name: "Ajay",
+  name: "Ajay Karkede",
   health: 50,
   heal(a, b) {
     return (this.health += a + b);
@@ -15,21 +14,51 @@ const archer = {
   name: "Robin Hood",
   health: 30,
 };
-console.log("1", archer);
-wizard.heal.call(archer, 20, 30); // call we can pass as parameter  and apply passing as parameter we can as array data structure
-wizard.heal.apply(archer, [10, 15]);
-console.log("2", archer);
-let healArcher = wizard.heal.bind(archer, 10, 25); // take as parameter and retrun as function not call imdiatelly call so you call later when ever you need that
+// console.log("1", archer);
+wizard.heal.call(archer, 40, 30); // call take parameters, parameters  comma sperated
+// console.log("2", archer);
+wizard.heal.apply(archer, [10, 55,]); // apply take array of parameters  this is only difference between  call and apply;
+// console.log("3", archer);
+let healArcher = wizard.heal.bind(archer, 100, 25); // take just like call  parameter and retrun as function not call imdiatelly call so you call later when ever you need that
 healArcher();
-console.log("3", archer);
+// console.log("4", archer);
+//  Note ===> call and apply useful for browroning methods from objects  and bind useful calling a function later like certain context
 
-//  function currying
-
+// function currying
 function multiply(a, b) {
   return a * b;
 }
 
-let multiplyByTwo = multiply.bind(this, 2);
-console.log(multiplyByTwo(6));
-let multiplyByTen = multiply.bind(this, 10);
-console.log(multiplyByTen(5));
+// let multiplyByTwo = multiply.bind(this, 2);
+// console.log(multiplyByTwo(6));
+// let multiplyByTen = multiply.bind(this, 10);
+// console.log(multiplyByTen(5));
+
+//  function currying 
+
+function multipleByTwo(a, b) {
+  return a * b;
+}
+
+let two = multipleByTwo.bind(this, 2);
+console.log(two(4));
+// let ten = multipleByTen.bind(this, 10);
+// console.log(ten(5));
+
+//  exerice 
+var b = {
+  name: "Nitin",
+  say() { console.log("B", this) },
+};
+
+var c = {
+  name: "Anand",
+  say() { return function () { console.log("c", this) } },
+};
+
+var d = {
+  name: "Ajay",
+  say() { return () => console.log("D", this) },
+};
+
+d.say()();
