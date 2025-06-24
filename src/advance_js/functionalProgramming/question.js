d12538d0d0c059031f3ccf4c81718ea8bb363c7c
Raushan kumar <raushan.kumar@tappp.com>
2025-06-25 02:12:00 +0530

    functional programming

---
+++ src/advance_js/functionalProgramming/question.js
@@ -0,0 +1,18 @@
+//  funcrion 
+//  Amazon shopping 
+const user = {
+    name: "Ajay",
+    active: true,
+    cart: [],
+    purchases: [],
+};
+
+//  Implement the cart features
+//  1. Add item to carts
+// 2. Add 3% tax to item in cart
+// 3. Buy item : cart ===>  purchases
+//  4. Empty cart
+
+//  Bouns :
+//  Accept refunds.
+//  Track user History.
